19ebe48fe24963f1915e9e21b3fd63fbb72199ab
Aquindesay <danielrei20179@gmail.com>
2024-12-02 15:36:47 +0000

    1

---
+++ Day2/Day2.py
@@ -0,0 +1,12 @@
+
+data = open("Day2/PuzzelInputDay2.txt", "r")
+
+def issort(liste):
+    for i in range(0, len(liste)- 1):
+        if liste 
+
+def main(liste):
+    issort(liste)
+
+
+print(data.read(1))
