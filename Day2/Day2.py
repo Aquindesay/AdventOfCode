19d26f14fcb91dfca04902b4eff13d3e4c52e6e6
Aquindesay <danielrei20179@gmail.com>
2024-12-05 15:00:33 +0000

    2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,12 +1,30 @@
 
-data = open("Day2/PuzzelInputDay2.txt", "r")
+data = open("Day2/PuzzelInputDay2.txt", "r").readlines()
 
-def issort(liste):
-    for i in range(0, len(liste)- 1):
-        if liste 
+distance = 0
+liste = []
+count = 0
 
-def main(liste):
-    issort(liste)
+for item in data:
+    liste.append(list(map(int,item.strip("\n").split())))
+
+for tem in liste:
+    for i in range(0, len(tem)-1):
+        distance = abs(tem[i] - tem[i+1])
+        if distance > 3:
+            break
+    curr = list(tem)
+    tem.sort()
+    if curr == tem:
+        count += 1
+    tem.sort(reverse = True)
+    if curr == tem:
+        count += 1     
+        
+print(count)
+        
 
 
-print(data.read(1))
+
+
+
